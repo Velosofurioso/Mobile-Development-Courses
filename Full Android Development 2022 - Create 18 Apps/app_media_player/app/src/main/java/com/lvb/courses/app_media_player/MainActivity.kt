ae0da2979c494e370a15a00b1f793d01181a0c0a
Lucas Veloso <l.brando815@gmail.com>
2023-03-08 23:16:31 -0300

    Finishing app_media_player

--- Full Android Development 2022 - Create 18 Apps/app_media_player/app/src/main/java/com/lvb/courses/app_media_player/MainActivity.kt
+++ Full Android Development 2022 - Create 18 Apps/app_media_player/app/src/main/java/com/lvb/courses/app_media_player/MainActivity.kt
@@ -1,37 +1,95 @@
 package com.lvb.courses.app_media_player
 
+import android.content.Context
+import android.media.AudioManager
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import com.lvb.courses.app_media_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mediaPlayer : MediaPlayer
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var audioManager: AudioManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         this.mediaPlayer = MediaPlayer.create(applicationContext, R.raw.teste)
+
+        initSeekBar()
     }
 
     fun executeSounds(view: View) {
-        mediaPlayer?.start()
+        if (this::mediaPlayer.isInitialized && mediaPlayer.isPlaying) {
+            mediaPlayer.start()
+            this.mediaPlayer = MediaPlayer.create(applicationContext, R.raw.teste)
+        }
     }
 
     fun pauseSounds(view: View) {
-        mediaPlayer?.let {
-            if (it.isPlaying)
-                it.pause()
+        if (this::mediaPlayer.isInitialized && mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+            this.mediaPlayer = MediaPlayer.create(applicationContext, R.raw.teste)
         }
     }
 
     fun stopSounds(view: View) {
-        mediaPlayer?.let {
-            it.stop()
+        if (this::mediaPlayer.isInitialized && mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
             this.mediaPlayer = MediaPlayer.create(applicationContext, R.raw.teste)
-
         }
     }
+
+    private fun initSeekBar() = with(binding) {
+
+        // configure audio manager
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        //Recover values of maximum volume and actual volume
+        val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        val actualVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+
+        //Configure the maximum value for seekbar
+        seekVolume.max = maxVolume
+
+        //Configure the actual value for seekbar
+        seekVolume.progress = actualVolume
+
+        seekVolume.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, p1, 0)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (this::mediaPlayer.isInitialized && mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (this::mediaPlayer.isInitialized && mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+            mediaPlayer.release() // Release media resources that the media player is using
+        }
+    }
+
 }
